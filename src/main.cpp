6c57df9c6d0993e569966fe42938166e66fce0ac
Michael Witrant <michael@ouvrages-web.fr>
2015-09-26 09:23:18 +0200

    Do not create block if no kernel was found
    Before this change a block was always created even if no Proof of Stake
    kernel was found.

--- src/main.cpp
+++ src/main.cpp
@@ -5407,6 +5407,9 @@ CBlockTemplate* CreateNewBlock(CReserveKey& reservekey, CWallet* pwallet, bool f
             nLastCoinStakeSearchInterval = nSearchTime - nLastCoinStakeSearchTime;
             nLastCoinStakeSearchTime = nSearchTime;
         }
+        // nubit: no kernel found, do not bother creating the block
+        if (!pblock->IsProofOfStake())
+            return NULL;
     }
 
     pblock->nBits = GetNextTargetRequired(pindexPrev, pblock->IsProofOfStake());
@@ -5847,7 +5850,10 @@ void BitcoinMiner(CWallet *pwallet, bool fProofOfStake)
 
         auto_ptr<CBlockTemplate> pblocktemplate(CreateNewBlock(reservekey, pwallet, fProofOfStake));
         if (!pblocktemplate.get())
-            return;
+        {
+            MilliSleep(500);
+            continue;
+        }
         CBlock *pblock = &pblocktemplate->block;
         IncrementExtraNonce(pblock, pindexPrev, nExtraNonce);
 
